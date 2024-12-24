7cb341ba5f7055420d4eff3eebce92bc9d77f0c4
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2024-12-24 13:43:05 +0300

    move and laser

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,22 @@
+using Unity.Mathematics;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange = 5f;
+    [SerializeField] float yClampRange = 5f;
+    [SerializeField] float controlRollFactor = 30f;
+    [SerializeField] float controlPitchFactor = 30f;
+    [SerializeField] float rotationSpeed = 5f;
     
     Vector2 movement;
     void Update()
     {
         ProcessThrusting();
+        ProcessRotation();
     }
 
     public void OnMove(InputValue value)
@@ -17,9 +25,27 @@ public class PlayerMovement : MonoBehaviour
     }
     private void ProcessThrusting()
     {
+        //movement on X-axis
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawXpos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(rawXpos, -xClampRange, xClampRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        //movement on Y-axis
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYpos = transform.localPosition.y + yOffset;
+        float clampedYpos = Mathf.Clamp(rawYpos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, 0f);
+    }
+
+    private void ProcessRotation()
+    {
+        float roll = -controlRollFactor * movement.x;
+        float pitch = -controlPitchFactor * movement.y;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+
     }
 }
