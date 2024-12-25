500b2603bc1b69e1bbbbfea07cc96e3ac8ba7f80
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2024-12-25 15:52:44 +0300

    destroyVFX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedVFX;
     void OnParticleCollision()
     {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
